99d5f310739de129c40f4be1b68f3c8dd295b8b4
Mark Teekman <mark.teekman@gmail.com>
2022-10-17 13:19:09 +0200

    Add Astro Tailwind integration

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+// prettier.config.js
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+}
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://markteekman.github.io',
-  base: '/frontend-mentor-challenge-starter'
+  base: '/frontend-mentor-challenge-starter',
+  integrations: [tailwind()]
 })
